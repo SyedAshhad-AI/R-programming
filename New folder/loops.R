3b5cb9f8d6b94ea95e0c16c40357d448b60a9f92
SyedAshhad-AI <ashhad12009@gmail.com>
2025-02-21 20:47:50 +0500

    Loops

---
+++ New folder/loops.R
@@ -0,0 +1,43 @@
+
+
+i <- 1
+sum <- 0
+
+while ( i <= 10 ) {
+  print (i)
+  sum = sum + i
+  i = i+1
+}
+print(sum)
+
+table <- 8
+a <- 1 
+while ( a <= 10 ) {
+  print(table)
+  print ( a * table)
+  a = a + 1
+}
+
+i <- 1
+sum <- 1
+fact <- 7
+while ( i <= fact ) {
+  print (i)
+  sum = sum * i
+  i = i+1
+}
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
